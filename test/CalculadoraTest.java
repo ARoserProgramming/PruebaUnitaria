5aa7d3d23af0163fd5d793e6b21b89685272cdae
AlejandroRoser <105304826+AlejandroRoser@users.noreply.github.com>
2024-03-27 09:30:52 +0100

    Add files via upload

--- test/CalculadoraTest.java
+++ test/CalculadoraTest.java
@@ -8,6 +8,7 @@ class CalculadoraTest {
     @Test
     void sumar() {
         assertEquals(5, Calculadora.sumar(2, 3));
+        assertEquals(-4, Calculadora.sumar(-2, -2));
         assertEquals(-1, Calculadora.sumar(-2, 1));
         assertEquals(0, Calculadora.sumar(0, 0));
         assertEquals(10, Calculadora.sumar(5, 5));
@@ -16,6 +17,7 @@ class CalculadoraTest {
     @Test
     void restar() {
         assertEquals(-1, Calculadora.restar(2, 3));
+        assertEquals(1, Calculadora.restar(-2, -3));
         assertEquals(-3, Calculadora.restar(-2, 1));
         assertEquals(0, Calculadora.restar(5, 5));
         assertEquals(5, Calculadora.restar(10, 5));
@@ -24,6 +26,7 @@ class CalculadoraTest {
     @Test
     void multiplicar() {
         assertEquals(6, Calculadora.multiplicar(2, 3));
+        assertEquals(6, Calculadora.multiplicar(-3,-2));
         assertEquals(-2, Calculadora.multiplicar(-1, 2));
         assertEquals(0, Calculadora.multiplicar(0, 5));
         assertEquals(100, Calculadora.multiplicar(10, 10));
@@ -33,6 +36,7 @@ class CalculadoraTest {
     void dividir() {
         assertEquals(2.5, Calculadora.dividir(5, 2));
         assertEquals(-2, Calculadora.dividir(-4, 2));
+        assertEquals(2, Calculadora.dividir(-4, -2));
         assertEquals(Double.POSITIVE_INFINITY, Calculadora.dividir(5, 0));
         assertEquals(0, Calculadora.dividir(0, 5));
     }
